90376d5758b9929e8afaccdfcde1c4179ac4b565
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2022-11-13 20:55:07 +0300

    Add some logic to class game

--- src/game.js
+++ src/game.js
@@ -5,8 +5,8 @@ export default class Game {
 
     playfield = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -30,11 +30,53 @@ export default class Game {
         x: 0,
         y: 0,
         blocks: [
-        [0, 1, 0],
-        [1, 1, 1],
-        [0, 0, 0]
-    ],
-};
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ],
+    };
+
+    movePieceLeft() {
+        this.activePiece.x -= 1;
+
+        if (this.isPieceeOutOfBounds()) {
+            this.activePiece.x += 1;
+        };
+    };
+
+    movePieceRight() {
+        this.activePiece.x += 1;
+
+        if (this.isPieceeOutOfBounds()) {
+            this.activePiece.x -= 1;
+        };
+    };
+
+    movePieceDown() {
+        this.activePiece.y += 1;
+
+        if (this.isPieceeOutOfBounds()) {
+            this.activePiece.y -= 1;
+            this.lockPiece();
+        };
+    };
+
+    isPieceeOutOfBounds() {
+        const playfield = this.playfield;
+        const { y, x } = this.activePiece;
+
+        return playfield[y] === undefined || this.playfield[y][x] === undefined;
+    };
+
+    lockPiece() {
+        const { y: row, x: col, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                this.playfield[row + y][col + x] = this.activePiece.blocks[y][x];
+            };
+        };
+    };
 
 
 }
